339d1e31f87d82cfd66d844ee16ec7142df89571
Magwit <magnushakon@hotmail.com>
2020-01-21 15:57:04 +0200

    Add test for daytrips tab

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
+import pytest
 from selenium.webdriver import Chrome
 
 # Fixture to initialize and quit for all tests
 @pytest.fixture
 def browser(scope='module'):
+    print("initiating chrome driver")
     driver = Chrome
     driver.implicitly_wait(10)
     yield driver
+    print("quitting chrome driver")
     driver(quit)
---
+++ test_daytrips_tab.py
@@ -0,0 +1,5 @@
+# GIVEN the user is on The Official Tourist website for Prague
+
+# WHEN the userclicks on 'Day Trips' in the top menu
+
+# THEN the user is on a new tab with information on day trips around Prague
