b7a276cbb7cd995624da2037316daf960e24a5c4
Magwit <magnushakon@hotmail.com>
2020-02-03 19:23:31 +0100

    Remove implicit wait in conftest for performance improvement

--- test_daytrips_tab.py
+++ test_daytrips_tab.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_daytrips_tab_functionality(browser):
     # GIVEN the user is on The Official Tourist website for Prague
+
     base_url = "https://www.prague.eu/en"
 
     browser.maximize_window()
@@ -13,7 +14,7 @@ def test_daytrips_tab_functionality(browser):
     print(browser.current_url)
 
     # WHEN the user clicks on 'Day Trips' in the top menu
-    # NOTE use pytest -rP to display print statements
+
     wait = WebDriverWait(browser, 20)
     wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Day Trips")))
     day_trip_menu_item = browser.find_element(By.LINK_TEXT, "Day Trips")
@@ -21,7 +22,7 @@ def test_daytrips_tab_functionality(browser):
     # TODO look into try expect
 
     # THEN the user is on a new tab with information on day trips around Prague
-    # assertion
+
     # https://www.browserstack.com/guide/python-selenium-to-run-web-automation-test
     # http://allselenium.info/handling-multiple-windows-python-selenium/
     browser_tabs = browser.window_handles
